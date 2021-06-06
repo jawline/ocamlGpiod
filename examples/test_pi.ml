b33c4b0392f3ebf5a350b3dc8b63ad94a76f772e
Blake Loring <blake@parsed.uk>
2021-06-07 00:35:58 +0800

    Add an example of Raspbery-Pi usage for the library

---
+++ examples/test_pi.ml
@@ -0,0 +1,27 @@
+open Printf
+
+exception InternalError
+
+let check r =
+  if Gpiod.is_null r then raise InternalError
+;;
+
+let gpio_test_pi () =
+  printf "preparing call\n";
+  let s = (Gpiod.version_string ()) in
+  printf "done %s\n" s;
+  let chip = Gpiod.chip_open "gpiochip0" in
+  check chip;
+  let lineRed = Gpiod.chip_get_line chip 24 in
+  let lineYellow = Gpiod.chip_get_line chip 25 in
+  check lineRed;
+  check lineYellow;
+  printf "Finished setting up\n";
+  let _ = Gpiod.line_request_output lineRed "example1" 0 in
+  let _ = Gpiod.line_request_output lineYellow "example1" 0 in
+  let _ = Gpiod.line_set_value lineRed 1 in
+  let _ = Gpiod.line_set_value lineYellow 1 in
+  ()
+;;
+
+let () = gpio_test_pi () ;;
